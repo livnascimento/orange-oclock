2fbec1965f9085728db0e1be2bd45fc84fb3f4bb
Zandra Maria De Almeida Vieitez <z.de.almeida.vieitez@avanade.com>
2024-05-21 10:49:02 -0300

    Refactor: espacamento

--- ProjetoMyTeDev/Models/Funcionario.cs
+++ ProjetoMyTeDev/Models/Funcionario.cs
@@ -15,6 +15,26 @@ namespace ProjetoMyTeDev.Models
         [Display(Name = "Departamento")]
         public string? DeptoFunc { get; set; }
 
+        [Required]
+        public string? FuncionarioNome { get; set; }
+
+        [EmailAddress(ErrorMessage = "O campo E-mail não é um endereço válido.")]
+        [Display(Name = "E-mail")]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        [Display(Name = "Departamento")]
+        [Required]
+        public int DepartamentoId { get; set; }
+        public Departamento Departamento { get; set; }
+
+        [Display(Name = "Nível de Acesso")]
+        [Required]
+        public int NivelAcessoId { get; set; }
+
         [Display(Name = "Data de Contratação")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime? DtContratacao { get; set; }
