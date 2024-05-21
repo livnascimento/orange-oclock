75b0659afbb9ab8d2e50a9728b3c9448e1ff256d
Zandra Maria De Almeida Vieitez <z.de.almeida.vieitez@avanade.com>
2024-05-21 10:53:27 -0300

    Fix: merge

--- ProjetoMyTeDev/Models/Funcionario.cs
+++ ProjetoMyTeDev/Models/Funcionario.cs
@@ -26,26 +26,6 @@ namespace ProjetoMyTeDev.Models
         public int NivelAcessoId { get; set; }
         public NivelAcesso NivelAcesso { get; set; }
 
-        [Required]
-        public string? FuncionarioNome { get; set; }
-
-        [EmailAddress(ErrorMessage = "O campo E-mail não é um endereço válido.")]
-        [Display(Name = "E-mail")]
-        [Required]
-        public string Email { get; set; }
-
-        [Required]
-        public string Senha { get; set; }
-
-        [Display(Name = "Departamento")]
-        [Required]
-        public int DepartamentoId { get; set; }
-        public Departamento Departamento { get; set; }
-
-        [Display(Name = "Nível de Acesso")]
-        [Required]
-        public int NivelAcessoId { get; set; }
-
         [Display(Name = "Data de Contratação")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime? DataContratacao { get; set; }
