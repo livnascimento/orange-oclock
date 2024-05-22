f637a4637719558a89de3a14c4386fb0f5a6b6ef
Isabella Regina <isabellaregina344@gmail.com>
2024-05-22 11:52:34 -0300

    refactor: view Index pasta Identity

--- ProjetoMyTeDev/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ ProjetoMyTeDev/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -29,6 +29,7 @@ namespace ProjetoMyTeDev.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [Display(Name = "Usuário")]
         public string Username { get; set; }
 
         /// <summary>
@@ -56,7 +57,7 @@ namespace ProjetoMyTeDev.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Phone]
-            [Display(Name = "Phone number")]
+            [Display(Name = "Telefone")]
             public string PhoneNumber { get; set; }
         }
 
