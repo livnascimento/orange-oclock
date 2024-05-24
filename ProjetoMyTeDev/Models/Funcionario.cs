6350e3883776932820753b59ffc68e4a3d04ca7d
deborahpaz <deborah.bpaz@gmail.com>
2024-05-24 10:48:47 -0300

    Refactor: Funcionalidade no relatório - View SomenteAdmin

--- ProjetoMyTeDev/Models/Funcionario.cs
+++ ProjetoMyTeDev/Models/Funcionario.cs
@@ -1,8 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoMyTeDev.Models
 {
-    public class Funcionario
+    public class  Funcionario
     {
         public int FuncionarioId { get; set; }
 
