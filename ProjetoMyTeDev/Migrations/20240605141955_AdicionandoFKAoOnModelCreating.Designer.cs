b338cce0fc48a40c94e954451960d61882f9f011
Lívia <livia.do.nascimento@avanade.com>
2024-06-06 12:11:08 -0300

    refactor: troca de tipo da Data

--- ProjetoMyTeDev/Migrations/20240605141955_AdicionandoFKAoOnModelCreating.Designer.cs
+++ ProjetoMyTeDev/Migrations/20240605141955_AdicionandoFKAoOnModelCreating.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoMyTeDev.Data;
 
@@ -11,9 +12,11 @@ using ProjetoMyTeDev.Data;
 namespace ProjetoMyTeDev.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605141955_AdicionandoFKAoOnModelCreating")]
+    partial class AdicionandoFKAoOnModelCreating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
